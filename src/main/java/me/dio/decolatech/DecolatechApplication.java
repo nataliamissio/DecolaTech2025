7d4bfeb90094dd0ca12eb4f384b72552b4174785
natalia <nataalia.missio@gmail.com>
2025-02-24 20:48:37 -0300

    Implementação da camada de dominio

---
+++ src/main/java/me/dio/decolatech/DecolatechApplication.java
@@ -0,0 +1,15 @@
+package me.dio.decolatech;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DecolatechApplication {
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(DecolatechApplication.class, args);
+		System.out.println(("aaaa"));
+	}
+
+}
